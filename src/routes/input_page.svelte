7f8216adafefad4890e2967e67bdd78fda9278cf
Rafayel Latif <rafayel.latif@gmail.com>
2024-11-18 22:28:59 +0000

    Update endpoint

--- src/routes/input_page.svelte
+++ src/routes/input_page.svelte
@@ -156,7 +156,7 @@ async function generate(event) {
 
     isLoading = true; // Start showing the spinner
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/travel", {
+      const response = await fetch("https://my-svelte-project.onrender.com/api/travel", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(travelData)
