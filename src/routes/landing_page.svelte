bfa4bddb17d6904891b965d5ee3eac533ec55dab
Rafayel Latif <rafayel.latif@gmail.com>
2024-11-19 20:48:33 +0000

    Css changes

--- src/routes/landing_page.svelte
+++ src/routes/landing_page.svelte
@@ -1,16 +1,5 @@
 <script>
-  import { onMount } from 'svelte';
 
-  onMount(() => {
-    const script = document.createElement('script');
-    script.src = 'https://md-block.verou.me/md-block.js';
-    script.type = 'module';
-    script.onload = () => {
-      // You can now use <md-block> after the script is loaded
-      console.log('md-block script loaded');
-    };
-    document.head.appendChild(script);
-  });
 </script>
 
 <main>
