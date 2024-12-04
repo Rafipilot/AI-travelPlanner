2a790c0503868a09501592f0689bdee197b81e88
Rafayel Latif <rafayel.latif@gmail.com>
2024-12-04 16:53:21 +0000

    api changes

--- backend/api.py
+++ backend/api.py
@@ -333,9 +333,8 @@ def get_openai_response(number_of_people, departure, destination, duration,fligh
 
     f"**Restaurant Options based on your hotel location:"
     f"{restaurants}"
-
-    f"name"
-    f"  -url"
+    f"restaurant name"
+    f"restaurant website"
 
     f"**Activities and Attractions:**\n"
     f"- Based on the duration of the trip, suggest activities that are relevant to the destination. Maybe like 1-2 activites per day in a list format"
